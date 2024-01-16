c135a2262485f3e2a6d64798a3fd908ea663ebe4
hdev42 <128073814+hdev42@users.noreply.github.com>
2024-01-16 17:44:31 +0100

    code commit

---
+++ hassanAI/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for hassanAI project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hassanAI.settings')
+
+application = get_wsgi_application()
+
+app = application
